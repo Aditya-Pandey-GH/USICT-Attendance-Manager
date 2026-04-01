3df45fc3a3904176855525484d4ad83d7ee53862
Aditya Pandey <adityapandey1609@gmail.com>
2026-04-01 14:06:13 +0530

    Setup MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URL, "http://localhost:5500"],
+		credentials: true,
+	}),
+);
+
+// MongoDB Connection
+mongoose
+	.connect(process.env.MONGO_URI + "/" + process.env.MONGO_DB_NAME)
+	.then(() => {
+		console.log("MongoDB Connected");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 // Express Routes
 app.get("/", (req, res) => {
